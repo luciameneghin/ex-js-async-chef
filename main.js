e3de13980dc2c10c889322b170fc198c4dc487de
luciameneghin <lucia.meneghin00@gmail.com>
2025-04-03 17:28:13 +0200

    feat: returned chef's birth date

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('ciao')
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = await response.json();
+  return obj;
+}
+
+async function getChefBirthday(id) {
+  const recipes = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+  const user = await fetchJson(`https://dummyjson.com/users/${recipes.userId}`);
+  return { birthDate: user.birthDate };
+}
+
+getChefBirthday(1)
+  .then(birthDate => console.log(birthDate))
+  .catch(error => console.error('Error fetching data:', error));
